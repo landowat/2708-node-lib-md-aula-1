f8d92e4527d058a041563ed2b478d8af97501f34
owatanab <owatanab@nttdata.com>
2024-02-07 19:09:49 -0300

    dev: aula 2 - tratamento de erros, promises e async/await

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
+import fs from 'fs';
 import chalk from 'chalk';
-//encadear métodos para colorir texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold('Alura'));
-//receber múltiplos argumentos
-console.log(chalk.blue('curso', 'de', 'Node.js'));
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+
+function trataErro(erro) {
+    console.log(erro);
+
+    throw new Error(chalk.bgRed(erro.code, 'arquivo não encontrado no diretório informado.'));
+}
+
+// Sincrono
+function pegaArquivo(caminhoArquivo) {
+    const encoding = 'utf-8';
+
+    fs.readFile(caminhoArquivo, encoding, (erro,texto) => {
+        if(erro) {
+            trataErro(erro);
+        }
+
+        console.log(chalk.blueBright(texto))
+    })
+}
+
+// Assincrono - promoses com then
+function pegaArquivoAssinc(caminhoArquivo) {
+    const encoding = 'utf-8';
+
+    fs.promises.readFile(caminhoArquivo, encoding)
+        .then((texto) => console.log(chalk.greenBright(texto)))
+        .catch(trataErro)
+}
+
+// Assincrono - async await
+async function pegaArquivoAssincV2(caminhoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding);
+        console.log(chalk.yellowBright(texto));
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+// //encadear métodos para colorir texto, cor de fundo e texto em negrito
+// console.log(chalk.blue.bgWhite.bold('Alura'));
+// //receber múltiplos argumentos
+// console.log(chalk.blue('curso', 'de', 'Node.js'));
+// //métodos aninhados
+// console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+// // uso de template strings e placeholders
+// console.log(`
+// CPU: ${chalk.red('90%')}
+// RAM: ${chalk.green('40%')}
+// DISK: ${chalk.yellow('70%')}
+// `);
+
+pegaArquivo('./arquivos/texto.md');
+
+pegaArquivoAssinc('./arquivos/texto.md');
+
+pegaArquivoAssincV2('./arquivos/texto.md');
+
+pegaArquivoAssincV2('./arquivos/texto.md');
+pegaArquivoAssincV2('./arquivos/');
